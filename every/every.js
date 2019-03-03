21007834c5e89c74989da5795d780df53789f39d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-03 18:14:08 +0530

    every

---
+++ every/every.js
@@ -0,0 +1,9 @@
+function isBelowThreshold(currentValue) {
+    return currentValue < 40;
+  }
+  
+  var array1 = [1, 30, 39, 29, 10, 13];
+  
+  console.log(array1.every(isBelowThreshold));
+  // expected output: true
+  
