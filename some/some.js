c8697609f0e804934ce292037a1b7f7a05e03348
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-03 18:15:55 +0530

    some function

---
+++ some/some.js
@@ -0,0 +1,9 @@
+var array = [1, 2, 3, 4, 5];
+
+var even = function(element) {
+  // checks whether an element is even
+  return element % 2 === 0;
+};
+
+console.log(array.some(even));
+// expected output: true
